9037b8b7dc3d7915f67309a1d5040ef67a4fc008
Emiliano Neira <Emilianoneira_@hotmail.com>
2021-09-15 20:37:33 -0300

    Diseño Arquitectura

---
+++ Hotel.Infra.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
